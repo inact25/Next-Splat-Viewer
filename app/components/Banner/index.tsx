d21e231bd4468215546886d337cd50d56eb39281
anggaPixa <anggas25@live.com>
2024-06-08 02:28:49 +0700

    initial commit

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -10,7 +10,7 @@ const Index = ({title, description}:any) => {
             <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
                 <div className="mx-auto max-w-3xl text-center">
                     <h1
-                        className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+                        className="pb-5 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
                     >
                         {title}
                     </h1>
