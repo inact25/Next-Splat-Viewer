9dd46df4df784852cb3176718c60d5cc1a8b8a15
Angga Pixa <anggas25@live.com>
2024-06-08 13:25:05 +0700

    add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons:'/ico.png',
   title: "VISIONSHIFT",
   description: "Experience your world in a whole new light.",
 };
