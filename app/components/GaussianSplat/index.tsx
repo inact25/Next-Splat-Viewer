ad2ef601770ab1be775feb9012308634ef64793d
indra <indra.gunanda@gmail.com>
2024-07-22 20:11:24 +0700

    feat: fix issues

--- app/components/GaussianSplat/index.tsx
+++ app/components/GaussianSplat/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import * as GaussianSplats3D from '@mkkellogg/gaussian-splats-3d';
 import { Camera } from 'three';
--- app/world/[path]/page.tsx
+++ app/world/[path]/page.tsx
@@ -1,18 +1,24 @@
-'use client';
 import React from 'react';
-import { useParams } from 'next/navigation';
-import GaussianSplat from '@/app/components/GaussianSplat';
 import { getFile } from '@/app/actions/http';
+import dynamic from 'next/dynamic';
 
-const Page = async () => {
-  const params = useParams();
+const GaussianSplat = dynamic(() => import('@/app/components/GaussianSplat'), {
+  ssr: false,
+  loading: () => <div>Loading...</div>,
+});
+const Page = async (props: any) => {
+  const { params } = props;
   const id = params.path as string;
-  const loadFile = await getFile(id);
-  return (
-    <>
-      <GaussianSplat src={loadFile.responseObject} />
-    </>
-  );
+  try {
+    const loadFile = await getFile(id);
+    return (
+      <>
+        <GaussianSplat src={loadFile.responseObject} />
+      </>
+    );
+  } catch (error) {
+    return <div>Failed to load file</div>;
+  }
 };
 
 export default Page;
