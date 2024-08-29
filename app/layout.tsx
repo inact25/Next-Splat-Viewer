8ff841a426f96d7bee3a4be24fcf05c90f50a8e7
unknown <rambudiarto@gmail.com>
2024-08-30 01:34:54 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script src="https://launchar.app/sdk/v1?key=wq218Ks333kuBXLbT2XZ4hJjeLGGZMnN&redirect=true"/>
+        {/* <Script src="https://launchar.app/sdk/v1?key=wq218Ks333kuBXLbT2XZ4hJjeLGGZMnN&redirect=true"/> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
